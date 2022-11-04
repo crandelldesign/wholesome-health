ae3f2471c02c0a46fa31b0e28e5ab7d69e8667be
Matt Crandell <mrcrandell@gmail.com>
2022-11-03 23:00:06 -0400

    began repo

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { RouterView } from "vue-router";
+</script>
+
+<template>
+  <div class="site-container">
+    <RouterView />
+  </div>
+</template>
+
+<style lang="scss">
+@import "@/assets/scss/_reboot.scss";
+@import "@/assets/scss/_global.scss";
+</style>
---
+++ src/views/StyleGuideView.vue
@@ -0,0 +1,26 @@
+<script setup>
+import LogoVertical from "../components/LogoVertical.vue";
+</script>
+
+<template>
+  <div class="style-guide">
+    <header class="header">
+      <div class="logo-container">
+        <LogoVertical />
+      </div>
+    </header>
+    <h1>Style Guide</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  padding: 1rem;
+  .logo-container {
+    max-width: 300px;
+    height: auto;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
